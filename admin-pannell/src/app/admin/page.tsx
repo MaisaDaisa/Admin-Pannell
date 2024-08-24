5e4e560cc5fc19f0c52235cc453bfd2b926485ad
Nick <maisadzenika@gmail.com>
2024-08-24 14:30:00 +0400

    initial

---
+++ admin-pannell/src/app/admin/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from './_adminComponents/Sidebar'
+
+
+function page() {
+  return (
+    <div className='h-dvh'>
+      <Sidebar></Sidebar>
+    </div>
+  )
+}
+
+export default page
